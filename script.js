e5f557e512d5c8b8e0baa7f0306ad5ab39204647
christoc <christocdas0@gmail.com>
2022-04-23 18:21:02 +0530

    Url link edited

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var searchBar = document.getElementById("search-box");
 // make empty array
 // apply keyup function
 $.get(
-  "https://607e95f802a23c0017e8ba2f.mockapi.io/habib-admin",
+  "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D",
   function (url) {
     dataUrl = url;
     tableDataCreate(url);
